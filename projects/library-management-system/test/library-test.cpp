7eaa86f8471f200ba0676601dfde8e67e5bd8ef7
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-10-25 12:37:37 +0300

    test(projects/library): test findBookById metod of the Library class

--- projects/library-management-system/test/library-test.cpp
+++ projects/library-management-system/test/library-test.cpp
@@ -4,11 +4,13 @@ int main()
 {
 
     Library library;
+    cout << library.findBookById(1) << endl; // should return -1
+    library.addBook();
+    cout << library.findBookById(1) << endl; // should return 0
 
-   library.addBook();
-   library.addBook();
-   library.addBook();
-   
+    library.addBook();
+    library.addBook();
+
     for (int i = 0; i < library.getBooksList()->getSize(); i++)
     {
         library.getBooksList()->getElmByIndex(i)->printDetails();
