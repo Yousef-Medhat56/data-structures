3bf5fa08ddd8c5cdd403ba67f6481bd6e8329e3f
Yousef-Medhat56 <yousef.medhat564@gmail.com>
2023-10-25 13:39:29 +0300

    test(projects/library): test the borrowing books methods

--- projects/library-management-system/test/library-test.cpp
+++ projects/library-management-system/test/library-test.cpp
@@ -4,18 +4,32 @@ int main()
 {
 
     Library library;
-    cout << library.findBookById(1) << endl; // should return -1
     library.addBook();
-    cout << library.findBookById(1) << endl; // should return 0
-
     library.addBook();
     library.addBook(); // i will make this book title = "book 3"
+    library.addBook();
 
-    library.removeBook(2); //remove the 2nd book
- 
+    library.removeBook(2); // remove the 2nd book
+
     cout << library.searchForBook("book 3") << endl; // should return 1
 
-   library.displayAllBooks();
+    library.borrowBook(1);
+    library.borrowBook(4);
+
+    cout << "# Borrowed:" << endl;
+    library.displayBorrowedBooks();
+    cout << "# Available:" << endl;
+    library.displayAvailableBooks();
+
+    library.returnBook(1);
+
+    cout << "# Borrowed:" << endl;
+    library.displayBorrowedBooks();
+    cout << "# Available:" << endl;
+    library.displayAvailableBooks();
+
+    cout << "# All books:" << endl;
+    library.displayAllBooks();
 
     return 0;
 }
